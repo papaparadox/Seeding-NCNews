4f5db5c9203d5e6b02cea8b2b2e234d6961db4df
papaparadox <tymurholovko@gmail.com>
2025-03-07 16:43:17 +0000

    Added another util function that is adding another column to comments of article_id

--- db/seeds/utils.js
+++ db/seeds/utils.js
@@ -5,5 +5,20 @@ exports.convertTimestampToDate = ({ created_at, ...otherProperties }) => {
   return { created_at: new Date(created_at), ...otherProperties };
 };
 
+exports.updateComments = (commentData, articleData) => {
+  const obj = {};
+  articleData.forEach((article) => {
+    obj[article.title] = article.article_id;
+  });
+  return commentData.map((comment) => {
+    comment.article_id = obj[comment.article_title];
+    return comment;
+  });
+};
+//format the comments
+//takes the rows which will be article data. take the comments data.
+//need to check which article_id belongs to article_title
+//need to update the comments to have article_id instead of article_title
 
-
+// try to use ternary to check if id belongs to title
+//use normal if statement return the desired one
