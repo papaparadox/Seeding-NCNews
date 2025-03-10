5a85792144b721360800355c7bd880986e4a455f
papaparadox <tymurholovko@gmail.com>
2025-03-10 15:54:39 +0000

    Added a model function to select necessary columns from articles table

--- models/app.models.js
+++ models/app.models.js
@@ -17,4 +17,13 @@ function selectArticleByID(article_id) {
     });
 }
 
-module.exports = { selectAllTopics, selectArticleByID };
+function selectAllArticles() {
+  return db
+    .query(
+      "SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.comment_id)::INT AS comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id GROUP BY articles.article_id ORDER BY created_at DESC"
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+}
+module.exports = { selectAllTopics, selectArticleByID, selectAllArticles };
