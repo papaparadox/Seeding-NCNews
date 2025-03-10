10a7fe4d87104d602bcb3d209de336d03581b90a
papaparadox <tymurholovko@gmail.com>
2025-03-10 14:18:31 +0000

    Added a model function for selecting article by it's id

--- models/app.models.js
+++ models/app.models.js
@@ -6,4 +6,15 @@ function selectAllTopics() {
   });
 }
 
-module.exports = { selectAllTopics };
+function selectArticleByID(article_id) {
+  return db
+    .query("SELECT * FROM articles WHERE article_id = $1", [article_id])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Not found" });
+      }
+      return rows[0];
+    });
+}
+
+module.exports = { selectAllTopics, selectArticleByID };
