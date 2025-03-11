99a738853dfc99a5ff32b279d0a7cc3c671fb2ab
papaparadox <tymurholovko@gmail.com>
2025-03-11 09:06:07 +0000

    Changed an sql request to be ordered by created_at column descending

--- models/app.models.js
+++ models/app.models.js
@@ -29,7 +29,10 @@ function selectAllArticles() {
 
 function selectCommentsByID(article_id) {
   return db
-    .query("SELECT * FROM comments WHERE article_id = $1", [article_id])
+    .query(
+      "SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC",
+      [article_id]
+    )
     .then(({ rows }) => {
       if (rows.length === 0) {
         return Promise.reject({ status: 404, msg: "Not found" });
