9839f64f26e61d7fe753ab5098694c673c5c4659
papaparadox <tymurholovko@gmail.com>
2025-03-11 08:56:16 +0000

    Added a model function to select comments from comment table with matching article id

--- models/app.models.js
+++ models/app.models.js
@@ -26,4 +26,20 @@ function selectAllArticles() {
       return rows;
     });
 }
-module.exports = { selectAllTopics, selectArticleByID, selectAllArticles };
+
+function selectCommentsByID(article_id) {
+  return db
+    .query("SELECT * FROM comments WHERE article_id = $1", [article_id])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Not found" });
+      }
+      return rows;
+    });
+}
+module.exports = {
+  selectAllTopics,
+  selectArticleByID,
+  selectAllArticles,
+  selectCommentsByID,
+};
