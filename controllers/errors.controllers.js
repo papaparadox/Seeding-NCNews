81dc7f6164232b4865e3659834692fd1d2e47f5a
papaparadox <tymurholovko@gmail.com>
2025-03-11 13:44:51 +0000

    Added more cases for error handlers

--- controllers/errors.controllers.js
+++ controllers/errors.controllers.js
@@ -4,3 +4,13 @@ exports.handleCustomErrors = (err, request, response, next) => {
   }
   next(err);
 };
+
+exports.handlePsqlErrors = (err, request, response, next) => {
+  if (err.code === "22P02" || err.code === "23502") {
+    response.status(400).send({ status: 400, msg: "Invalid request" });
+  }
+  if (err.code === "23503") {
+    response.status(404).send({ status: 404, msg: "Not found" });
+  }
+  next(err);
+};
