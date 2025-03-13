896b524582087e3fa13235a82d4b86c4ef81a798
papaparadox <tymurholovko@gmail.com>
2025-03-13 14:14:34 +0000

    Refactored connection to be in a working condition

--- db/connection.js
+++ db/connection.js
@@ -2,22 +2,20 @@ const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || "development";
 
-const config = {};
-
-if (ENV === "production") {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
-}
-module.exports = new Pool(config);
-
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
-
-const db = new Pool();
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("No PGDATABASE or DATABASE_URL configured");
 } else {
   console.log(`Connected to ${process.env.PGDATABASE}`);
 }
+
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+const db = new Pool(config);
 
 module.exports = db;
