6611b47e432618e3fae5837269d1083a0135b351
papaparadox <tymurholovko@gmail.com>
2025-03-14 11:38:49 +0000

    Added a model function that updates comments table and returns an updated version of it

--- models/app.models.js
+++ models/app.models.js
@@ -150,6 +150,24 @@ function selectUsersByUsername(username) {
       return rows[0];
     });
 }
+
+function updateCommentByID(votes, comment_id) {
+  if (!isNaN(votes)) {
+    return db
+      .query(
+        "UPDATE comments SET votes = votes + $1 WHERE comment_id = $2 RETURNING *",
+        [votes, comment_id]
+      )
+      .then(({ rows }) => {
+        if (rows.length === 0) {
+          return Promise.reject({ status: 404, msg: "Not found" });
+        }
+        return rows[0];
+      });
+  } else {
+    return Promise.reject({ status: 400, msg: "Invalid request" });
+  }
+}
 module.exports = {
   selectAllTopics,
   selectArticleByID,
@@ -161,4 +179,5 @@ module.exports = {
   selectAllUsers,
   selectArticlesByTopic,
   selectUsersByUsername,
+  updateCommentByID,
 };
